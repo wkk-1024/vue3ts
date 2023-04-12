bdb02c72118741cfc41a981e44b572ec9dbc6b90
ikun <wangkun@zhiqicai.com>
2023-04-12 18:18:08 +0800

    滚动条修改

--- src/views/indexViews.vue
+++ src/views/indexViews.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+const state = ref({
+  count: 4
+})
+
+const doubleCount = computed({
+  get: () => state.value.count * 2,
+  set: (val) => state.value.count = val / 2
+})
+
+state.value.count = 5
+console.log(doubleCount);
 
 </script>
 <template>
--- src/views/menu/MenuIndex.vue
+++ src/views/menu/MenuIndex.vue
@@ -10,20 +10,84 @@ const getAll = async () => {
   menusItem.value = [...data.data]
 }
 
+const dialogVisible = ref(false)
 getAll()
 
 // 提取一级菜单
-const oneMenu = computed(() =>{
-  return menusItem.value?.filter((e)=>e.level === 0)
+const oneMenu = computed(() => {
+  return menusItem.value?.filter((e) => e.level === 0)
 })
-
 </script>
 <template>
   <h2>菜单管理</h2>
   <el-card class="box-card">
     <template #header>
       <div class="card-header">
-        <el-button>Operation button</el-button>
+        <el-button @click="dialogVisible = true">新增菜单</el-button>
+        <el-dialog
+          v-model="dialogVisible"
+          title="新增菜单"
+          width="70%"
+          :draggable="true"
+          :lock-scroll="true"
+        >
+          <el-scrollbar style="height: 100%">
+            <div class="scroll-content" style="max-height: 60vh; margin-right: 10px">
+              <el-form label-width="120px">
+                <el-form-item label="Activity name">
+                  <el-input />
+                </el-form-item>
+                <el-form-item label="Activity zone">
+                  <el-select placeholder="please select your zone">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                  </el-select>
+                </el-form-item>
+                <el-form-item label="Activity time">
+                  <el-col :span="11">
+                    <el-date-picker type="date" placeholder="Pick a date" style="width: 100%" />
+                  </el-col>
+                  <el-col :span="2" class="text-center">
+                    <span class="text-gray-500">-</span>
+                  </el-col>
+                  <el-col :span="11">
+                    <el-time-picker placeholder="Pick a time" style="width: 100%" />
+                  </el-col>
+                </el-form-item>
+                <el-form-item label="Instant delivery">
+                  <el-switch />
+                </el-form-item>
+                <el-form-item label="Activity type">
+                  <el-checkbox-group>
+                    <el-checkbox label="Online activities" name="type" />
+                    <el-checkbox label="Promotion activities" name="type" />
+                    <el-checkbox label="Offline activities" name="type" />
+                    <el-checkbox label="Simple brand exposure" name="type" />
+                  </el-checkbox-group>
+                </el-form-item>
+                <el-form-item label="Resources">
+                  <el-radio-group>
+                    <el-radio label="Sponsor" />
+                    <el-radio label="Venue" />
+                  </el-radio-group>
+                </el-form-item>
+                <el-form-item label="Activity form">
+                  <el-input type="textarea" />
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary">Create</el-button>
+                  <el-button>Cancel</el-button>
+                </el-form-item>
+              </el-form>
+            </div>
+          </el-scrollbar>
+          <template #footer>
+            <span class="dialog-footer">
+              <el-button>Cancel</el-button>
+              <el-button type="primary"> Confirm </el-button>
+            </span>
+          </template>
+        </el-dialog>
       </div>
     </template>
     <el-table :data="menusItem" border style="width: 100%">
@@ -43,4 +107,10 @@ const oneMenu = computed(() =>{
   </el-card>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card-header {
+  :deep(.el-dialog__body) {
+    padding: 0 10px;
+  }
+}
+</style>
